623954e82adb8b7c17b40928ff513883ce02b689
plus2012 <plus2015@aliyun.com>
2016-04-03 09:32:34 +0800

    Add 'back' icon in setting pages. Clear project.

--- client/templates/user-setting.js
+++ client/templates/user-setting.js
@@ -1,7 +1,5 @@
 // 用于检测几个下级菜单是否打开的本地变量
-Session.setDefault("settingAccount", false);
-Session.setDefault("settingOthers", false);
-Session.setDefault("settingPassword", false);
+Session.setDefault("settingStatus", "home");
 
 // 菜单项目
 // 账号
@@ -17,36 +15,53 @@ Session.setDefault("settingPassword", false);
 // 关于
 
 Template.userSetting.helpers({
-  settingPassword: function(){
-    return Session.get("settingPassword");
-  },
-  settingAccount: function(){
-    return Session.get("settingAccount");
-  },
-  settingOthers: function(){
-    return Session.get("settingOthers");
-  },
-  allSetting: function(){
-    return !Session.get("settingPassword") && !Session.get("settingAccount") && !Session.get("settingOthers");
-  },
-  getAllSetting: [
-    {text:"写在 js 中的设置"},
-    {text:"写在 js 中的设置"},
-    {text:"写在 js 中的设置"},
-    {text:"写在 js 中的设置"},
-  ]
+  settingItems: function(){
+    switch (Session.get("settingStatus")) {
+      case "account":
+      return [
+        {text: "邮箱"     , jsAct: "" },
+        {text: "学号"     , jsAct: "" },
+        {text: "密码"     , jsAct: "js-page-password" },
+      ];
+      case "password":
+      return [
+        {text: "更改密码"  , jsAct: "" },
+      ];
+      case "common":
+      return [
+        {text: "背景"    , jsAct: "" },
+      ];
+      default:
+      return [
+        {text: "账号"     , jsAct: "js-page-account" },
+        {text: "通知"     , jsAct: "" },
+        {text: "隐私"     , jsAct: "" },
+        {text: "通用"     , jsAct: "js-page-common" },
+        {text: "帮助与反馈", jsAct: "" },
+        {text: "关于"     , jsAct: "" },
+      ];
+    }
+  }
 });
 
 
 Template.userSetting.events({
-  'click .js-set-password': function() {
-    Session.set("settingPassword",!Session.get("settingPassword"));
-    Session.set("settingAccount",!Session.get("settingAccount"));
+  'click .js-page-password': function() {
+    Session.set("settingStatus", "password");
   },
-  'click .js-set-account': function() {
-    Session.set("settingAccount",!Session.get("settingAccount"));
+  'click .js-page-account': function() {
+    Session.set("settingStatus", "account");
   },
-  'click .js-set-others': function() {
-    Session.set("settingOthers",!Session.get("settingOthers"));
+  'click .js-page-common': function() {
+    Session.set("settingStatus", "common");
+  },
+  'click .js-page-back': function() {
+    var backPage = {
+      "home":"home",
+      "password":"account",
+      "common":"home"
+    };
+    var status = Session.get("settingStatus")
+    Session.set("settingStatus",backPage[status]);
   }
 });
