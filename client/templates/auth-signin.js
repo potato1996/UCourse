52ca74c1348e9191525eea9aa09d7cf4990ac6bd
samsuzhang <samsuzhang@163.com>
2016-04-05 19:41:30 +0800

    recent & route

--- client/templates/auth-signin.js
+++ client/templates/auth-signin.js
@@ -40,7 +40,89 @@ Template.signin.events({
         return Session.set(ERRORS_KEY, {'none': error.reason});
       }
       
+      recentInit();
+      
       Router.go('home');
     });
   }
 });
+
+/*
+ * added by Sam, 2016.03.29, start
+ */
+function recentInit() {
+  var recent_name = "Recent";
+  var show_num = 5;
+
+  var lists = Lists.find({name: recent_name}).fetch();
+  var lists_num = Lists.find({name: recent_name}).count();
+  for (var i = 0; i < lists_num; i++) {
+    Meteor.subscribe('todos', lists[i]['_id']);
+    var todos = Todos.find({listId: lists[i]['_id']}).fetch();
+    var todos_num = Todos.find({listId: lists[i]['_id']}).count();
+    for (var j = 0; j < todos_num; j++) {
+      Todos.remove(todos[j]['_id']);
+    }
+    Lists.remove(lists[i]['_id']);
+  }
+
+  //var recent_date;
+  var recent = new Array();
+  var p = 0;
+  //var count = 0;
+  
+  var lists = Lists.find().fetch();
+  var lists_num = Lists.find().count();
+
+  for (var i = 0; i < lists_num; i++) {
+    Meteor.subscribe('todos', lists[i]['_id']);
+    var todos = Todos.find({listId: lists[i]['_id']}).fetch();
+    var todos_num = Todos.find({listId: lists[i]['_id']}).count();
+    //alert("id: " + lists[i]['_id'] + ", count: " + todos_num);
+    for (var j = 0; j < todos_num; j++) {
+      /*
+      if (count == 0) {
+        recent[p] = new Array();
+        recent[p]['text'] = todos[j]['text'];
+        recent[p++]['createdAt'] = todos[j]['createdAt'];
+        recent_date = todos[j]['createdAt'];
+      }
+      else if (todos[j]['createdAt'] >= recent_date) {
+        */
+        recent[p] = new Array();
+        recent[p]['text'] = todos[j]['text'];
+        recent[p++]['createdAt'] = todos[j]['createdAt'];
+        //recent_date = todos[j]['createdAt'];
+      //}
+      //count++;
+    }
+  }
+
+  recent.sort(function(b, a) {
+    return a['createdAt'] - b['createdAt'];
+  });
+
+
+
+  if (Lists.find({name: recent_name}).count() === 0) {
+    var list = {name: "Recent", incompleteCount: 0};
+    list._id = Lists.insert(list);
+
+    var n = p;
+    if (n > show_num)
+      n = show_num;
+
+    for (var i = 0; i < n; i++) {
+      Todos.insert({
+        listId: list._id,
+        text: recent[i]['text'],
+        checked: false,
+        createdAt: recent[i]['createdAt']
+      });
+      Lists.update(list._id, {$inc: {incompleteCount: 1}});
+    } 
+  }
+}
+/*
+ * added by Sam, 2016.03.29, end
+ */
