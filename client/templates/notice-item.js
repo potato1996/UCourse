21f93f3347c4d3aa30b87c29132dea16aa40f96b
zhaozewei <zhaozewei@pku.edu.cn>
2016-05-04 11:47:25 +0800

    Add notice time

--- client/templates/notice-item.js
+++ client/templates/notice-item.js
@@ -2,14 +2,21 @@
 //Session.setDefault("noticeTitle", "title");
 
 Template.noticeItem.helpers({
-  //showNotice: function(){
-    //return Session.get("noticeTitle");
-  //}
+  noticeTime: function(create_time) {
+    Y = create_time.getFullYear() + '-';
+    M = (create_time.getMonth()+1 < 10 ? '0'+(create_time.getMonth()+1) : create_time.getMonth()+1) + '-';
+    _D = create_time.getDate();
+    D = (_D < 10 ? '0'+ _D : _D) + ' ';
+    h = create_time.getHours() + ':';
+    m = create_time.getMinutes();
+    myTime = Y+M+D+h+m;
+    return myTime;
+  }
 });
 
 Template.noticeItem.events({
   'click .js-show-notice': function(text) {
     //Router.go('notice');
     //Session.set("noticeTitle", text);
-  }
+  },
 });
