314431cfd671377132d7a15d139d47369754eef0
plus2015 <plus2015@aliyun.com>
2016-05-19 11:27:16 +0800

    Add more course with files. Filesize may be wrong.

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -3,6 +3,257 @@
 
 var dev_courses = [
   {
+    coursename:"数学建模",
+    description:"数学建模是应用学科的核心内容，任何一门科学都是在数学的框架下表达自己解决问题的思想和方法，并和别的专业或者方向分享这些思想和方法。任何一门学科，只有当其使用数学时，才是好的精确的学科。",
+    notices:[
+      {
+        notificationname:"第一次作业",
+        description:"第一次作业已经发布，请大家在文件列表中查看！",
+        create_time:new Date(2016,3,18,18,0,0)
+      },{
+        notificationname:"第二次作业",
+        description:"第二次作业已经发布，请大家在列表中查看！",
+        create_time:new Date(2016,5,23,10,30,0)
+      }
+    ],
+    files:[
+      {
+        filename:"第 1 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/MM/01.pdf",
+        description:"第 1 周课件",
+        create_time:new Date(2016,2,22,10,0,0),
+        filetype:"pdf",
+        filesize:222
+      },
+      {
+        filename:"第 2 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/MM/02.pdf",
+        description:"第 2 周课件",
+        create_time:new Date(2016,3,1,10,0,0),
+        filetype:"pdf",
+        filesize:163
+      },
+      {
+        filename:"第 3 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/MM/03.pdf",
+        description:"第 3 周课件",
+        create_time:new Date(2016,3,8,10,0,0),
+        filetype:"pdf",
+        filesize:141
+      },
+      {
+        filename:"第 4 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/MM/04.pdf",
+        description:"第 4 周课件",
+        create_time:new Date(2016,3,15,10,0,0),
+        filetype:"pdf",
+        filesize:141
+      },
+      {
+        filename:"第 5 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/MM/05.pdf",
+        description:"第 5 周课件",
+        create_time:new Date(2016,3,23,10,0,0),
+        filetype:"pdf",
+        filesize:297
+      },
+      {
+        filename:"第 6 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/MM/06.pdf",
+        description:"第 6 周课件",
+        create_time:new Date(2016,4,1,10,0,0),
+        filetype:"pdf",
+        filesize:297
+      },
+      {
+        filename:"第 7 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/MM/07.pdf",
+        description:"第 7 周课件",
+        create_time:new Date(2016,4,8,10,0,0),
+        filetype:"pdf",
+        filesize:301
+      },
+      {
+        filename:"第 08 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/MM/08.pdf",
+        description:"第 08 周课件",
+        create_time:new Date(2016,5,6,10,0,0),
+        filetype:"pdf",
+        filesize:217
+      },
+    ]
+  },
+  {
+    coursename:"LINUX 程序设计",
+    description:"Linux（收听i英语发音：/ˈlɪnəks/ lin-əks）是一种自由和开放源代码的类UNIX操作系统。该操作系统的内核由林纳斯·托瓦兹在1991年10月5日首次发布。[5][6]，在加上用户空间的应用程序之后，成为Linux操作系统。Linux也是自由软件和开放源代码软件发展中最著名的例子。只要遵循GNU通用公共许可证，任何个人和机构都可以自由地使用Linux的所有底层源代码，也可以自由地修改和再发布。大多数Linux系统还包括像提供GUI的X Window之类的程序。除了一部分专家之外，大多数人都是直接使用Linux发行版，而不是自己选择每一样组件或自行设置。",
+    notices:[
+      {
+        notificationname:"大作业 Deadline",
+        description:"期末大作业请在 6.20 前提交！",
+        create_time:new Date(2016,5,18,18,0,0)
+      },{
+        notificationname:"系统安装准备",
+        description:"请大家在下周上课前自行安装 CentOS 7 发行版，可以使用虚拟机。",
+        create_time:new Date(2016,2,23,10,30,0)
+      }
+    ],
+    files:[
+      {
+        filename:"第 1 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/01.pdf",
+        description:"第 1 周课件",
+        create_time:new Date(2016,2,22,10,0,0),
+        filetype:"pdf",
+        filesize:222
+      },
+      {
+        filename:"第 2 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/02.pdf",
+        description:"第 2 周课件",
+        create_time:new Date(2016,3,1,10,0,0),
+        filetype:"pdf",
+        filesize:163
+      },
+      {
+        filename:"第 3 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/03.pdf",
+        description:"第 3 周课件",
+        create_time:new Date(2016,3,8,10,0,0),
+        filetype:"pdf",
+        filesize:141
+      },
+      {
+        filename:"第 4 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/04.pdf",
+        description:"第 4 周课件",
+        create_time:new Date(2016,3,15,10,0,0),
+        filetype:"pdf",
+        filesize:141
+      },
+      {
+        filename:"第 5 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/05.pdf",
+        description:"第 5 周课件",
+        create_time:new Date(2016,3,23,10,0,0),
+        filetype:"pdf",
+        filesize:297
+      },
+      {
+        filename:"第 6 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/06.pdf",
+        description:"第 6 周课件",
+        create_time:new Date(2016,4,1,10,0,0),
+        filetype:"pdf",
+        filesize:297
+      },
+      {
+        filename:"第 7 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/07.pdf",
+        description:"第 7 周课件",
+        create_time:new Date(2016,4,8,10,0,0),
+        filetype:"pdf",
+        filesize:301
+      },
+      {
+        filename:"第 08 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/08.pdf",
+        description:"第 08 周课件",
+        create_time:new Date(2016,5,6,10,0,0),
+        filetype:"pdf",
+        filesize:217
+      },
+      {
+        filename:"第 09 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/09.pdf",
+        description:"第 09 周课件",
+        create_time:new Date(2016,5,21,10,0,0),
+        filetype:"pdf",
+        filesize:299
+      },
+      {
+        filename:"第 10 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/LINUX/10.pdf",
+        description:"第 10 周课件",
+        create_time:new Date(2016,5,30,10,0,0),
+        filetype:"pdf",
+        filesize:299
+      },
+    ]
+  },
+  {
+    coursename:"信息论",
+    description:"信息论（英语：information theory）是应用数学、电机工程学和计算机科学的一个分支，涉及信息的量化。信息论是由克劳德·香农发展，用来找出信号处理操作的基本限制，如数据压缩、可靠的存储和数据传输的。自创立以来，它已拓展应用到许多其他领域，包括统计推断、自然语言处理、密码学、神经生物学[1]、进化论[2]和分子编码的功能[3]、生态学的模式选择[4]、热物理[5]、量子计算、语言学、剽窃检测[6]、模式识别、异常检测和其他形式的数据分析。[7]",
+    notices:[
+      {
+        notificationname:"讨论班通知",
+        description:"请大家即使参加周四晚的讨论班！",
+        create_time:new Date(2016,2,20,18,0,0)
+      },{
+        notificationname:"假期上课时间调整",
+        description:"考虑到教学进度，劳动节停掉的课在下周周日晚 8：30 补课！",
+        create_time:new Date(2016,4,37,10,30,0)
+      }
+    ],
+    files:[
+      {
+        filename:"第 1 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/IT/W01.pdf",
+        description:"第 1 周课件",
+        create_time:new Date(2016,2,22,10,0,0),
+        filetype:"pdf",
+        filesize:222
+      },
+      {
+        filename:"第 2 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/IT/W02.pdf",
+        description:"第 2 周课件",
+        create_time:new Date(2016,3,1,10,0,0),
+        filetype:"pdf",
+        filesize:163
+      },
+      {
+        filename:"第 3 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/IT/W03.pdf",
+        description:"第 3 周课件",
+        create_time:new Date(2016,3,8,10,0,0),
+        filetype:"pdf",
+        filesize:141
+      },
+      {
+        filename:"第 5 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/IT/W05.pdf",
+        description:"第 5 周课件",
+        create_time:new Date(2016,3,23,10,0,0),
+        filetype:"pdf",
+        filesize:297
+      },
+      {
+        filename:"第 7 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/IT/W07.pdf",
+        description:"第 7 周课件",
+        create_time:new Date(2016,4,8,10,0,0),
+        filetype:"pdf",
+        filesize:301
+      },
+      {
+        filename:"第 11 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/IT/W11.pdf",
+        description:"第 11 周课件",
+        create_time:new Date(2016,5,6,10,0,0),
+        filetype:"pdf",
+        filesize:217
+      },
+      {
+        filename:"第 13 周课件",
+        path:"http://121.42.173.75:9000/uc-doc/IT/W13.pdf",
+        description:"第 13 周课件",
+        create_time:new Date(2016,5,21,10,0,0),
+        filetype:"pdf",
+        filesize:299
+      },
+    ]
+  },
+  {
     coursename:"软件工程",
     description:"软件工程（英语：Software Engineering[1]）1968年秋季，NATO（北約）的科技委員會召集了近50名一流的編程人員、計算機科學家和工業界巨頭，討論和制定擺脫“軟體危機”的對策。在那次會議上第一次提出了軟體工程（software engineering）這個概念，研究和应用如何以系统性的、规范化的、可定量的过程化方法去开发和维护软件，以及如何把经过时间考验而证明正确的管理技术和当前能够得到的最好的技术方法结合起来的学科。它涉及到程序设计语言、数据库、软件开发工具、系统平台、标准、设计模式等方面。",
     notices:[
@@ -44,7 +295,8 @@ var dev_courses = [
         filesize:924
       }
     ]
-  },{
+  },
+  {
     coursename:"计算概论",
     description:"计算概论A是针对“信息科学技术学科一年级本科生”开设的一门专业基础主干课程。本课程的教学目标有二：其一，帮助学习者建立起“学习计算机科学技术知识所需的基本知识背景”；其二，帮助学习者“掌握计算机程序设计的基础知识”，培养学习者“独立设计计算机程序解决问题”的基本技能。",
     notices:[
@@ -75,7 +327,8 @@ var dev_courses = [
         filesize:1020
       },
     ]
-  },{
+  },
+  {
     coursename:"模拟电路",
     description:"模拟电路（英语：analogue electronics，美式：analog electronics）是涉及连续函数形式模拟信号的电子电路，与之相对的是数字电路，后者通常只关注0和1两个逻辑电平。“模拟”二字主要指电压（或电流）对于真实信号成比例的再现，它最初来源于希腊语词汇ανάλογος，意思是“成比例的”。",
     notices:[
@@ -95,7 +348,8 @@ var dev_courses = [
         filesize:1020
       },
     ]
-  },{
+  },
+  {
     coursename:"数字电路",
     description:"数字电路或数字集成电路是由许多的逻辑门组成的复杂电路。与模拟电路相比，它主要进行数字信号的处理（即信号以0与1两个状态表示），因此抗干扰能力较强。数字集成电路有各种门电路、触发器以及由它们构成的各种组合逻辑电路和时序逻辑电路。一个数字系统一般由控制部件和运算部件组成，在时脉的驱动下，控制部件控制运算部件完成所要执行的动作。通过模拟数字转换器、数字模拟转换器，数字电路可以和模拟电路互相连接。",
     notices:[
@@ -115,7 +369,8 @@ var dev_courses = [
         filesize:1020
       },
     ]
-  },{
+  },
+  {
     coursename:"软件工程实习",
     description:"软件工程（英语：Software Engineering[1]）1968年秋季，NATO（北約）的科技委員會召集了近50名一流的編程人員、計算機科學家和工業界巨頭，討論和制定擺脫“軟體危機”的對策。在那次會議上第一次提出了軟體工程（software engineering）這個概念，研究和应用如何以系统性的、规范化的、可定量的过程化方法去开发和维护软件，以及如何把经过时间考验而证明正确的管理技术和当前能够得到的最好的技术方法结合起来的学科。它涉及到程序设计语言、数据库、软件开发工具、系统平台、标准、设计模式等方面。",
     notices:[
