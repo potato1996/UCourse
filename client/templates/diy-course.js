8767eec2939c0ae9e2f3c34e64bfeb11a181eea6
zhaozewei <zhaozewei@pku.edu.cn>
2016-05-24 21:26:31 +0800

    Add error msg & router

--- client/templates/diy-course.js
+++ client/templates/diy-course.js
@@ -38,23 +38,35 @@ Template.DIYCourse.events({
   'submit': function(event, template) {
     event.preventDefault();
     
+    var url = template.$('[name=url]').val();
+    var account = template.$('[name=account]').val();
     var email = template.$('[name=email]').val();
     var password = template.$('[name=password]').val();
     
     var errors = {};
 
+    if (! url) {
+      errors.url = '请输入网址！';
+    }
+
+    if (! account) {
+      errors.account = '请输入帐号！';
+    }
+
     if (! email) {
-      errors.email = 'Email is required';
+      errors.email = '请输入邮箱！';
     }
 
     if (! password) {
-      errors.password = 'Password is required';
+      errors.password = '请输入密码！';
     }
     
     Session.set(ERRORS_KEY, errors);
     if (_.keys(errors).length) {
       return;
     }
+
+    Router.go('addCourse');
   },
 
   'click .js-close-notice': function() {
